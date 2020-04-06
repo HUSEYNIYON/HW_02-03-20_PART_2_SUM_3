ffec0962f0ef43823e164f76ea6a9756c005ad16
HUSEYNIYON <your>
2020-04-06 11:04:25 +0300

    Make a class Employee

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Employee
+    {
+        public string Name;
+        public string Surname;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
